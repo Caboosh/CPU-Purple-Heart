6a8aa13762afd08cf8a43e9011ed2450d87cdc25
Caboosh <cambomiller@googlemail.com>
2018-06-25 23:41:21 +0100

    update example bot config

--- botconfig-example.py
+++ botconfig-example.py
@@ -1,5 +1,6 @@
-# This is named the way it is so it doesnt conflict with an existing installation, and serves as a way to showcase the layout of this botconfig.py file
+# This is named the way it is so it doesn't conflict with an existing installation, and serves as a way to showcase the layout of this botconfig.py file
+# Rename this file to botconfig.py to use it and remove these comments
 
 prefix = 'Enter your Prefix here'
 token = 'Put your token here'
-botdesc = 'The Description of the bot if being used for other uses apart from being Nep'
+botdesc = '''The Description of the bot if being used for other uses apart from being Nep'''
