52eac810e599549c377c7a3d964bf63db2815300
Cameron Miller <cambomiller@googlemail.com>
2018-06-25 23:36:31 +0100

    Create configfile.py

---
+++ configfile.py
@@ -0,0 +1,5 @@
+# This is named the way it is so it doesnt conflict with an existing installation, and serves as a way to showcase the layout of this botconfig.py file
+
+prefix = 'Enter your Prefix here'
+token = 'Put your token here'
+botdesc = 'The Description of the bot if being used for other uses apart from being Nep'
