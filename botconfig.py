687738ec8d2826f2028bd6ab063630a53ce7f526
Cameron Miller <cambomiller@googlemail.com>
2018-06-27 13:48:03 +0100

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -1,4 +1,4 @@
 prefix = ''
 token = ''
-botdesc = 'A Discord based around the lovable Neptune and her 4th wall breaking abilities.
-She also does normal bot stuff, because y'know, that's important too right.'
+botdesc = '''A Discord based around the lovable Neptune and her 4th wall breaking abilities.
+She also does normal bot stuff, because y'know, that's important too right.'''
