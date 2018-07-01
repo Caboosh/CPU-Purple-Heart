7cbbf24ded4e08b09681a09ece8571fef9419654
Cameron Miller <cambomiller@googlemail.com>
2018-07-01 11:49:04 +0100

    added sphinx rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Cabooshy'
 author = 'Cabooshy'
 
 # The short X.Y version
-version = ''
+version = '0.2a'
 # The full version, including alpha/beta/rc tags
-release = '0.1a'
+release = '0.2a'
 
 
 # -- General configuration ---------------------------------------------------
@@ -74,7 +74,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
