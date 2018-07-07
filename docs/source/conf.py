de8052a542cb0c0232ea4119f78f34d00a48e01d
Cameron Miller <cambomiller@googlemail.com>
2018-07-07 12:28:56 +0100

    test for changing the theme bits

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,7 +86,10 @@ html_theme = 'classic'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "collapsiblesidebar": "true",
+    "bgcolor": "#7d0a68",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
