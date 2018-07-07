fded235753252b489f8e4e287abd7c81ba632f99
Cameron Miller <cambomiller@googlemail.com>
2018-07-07 11:49:51 +0100

    Remove all unnecessary parts from docs branch

--- botconfig-example.py
+++
@@ -1,6 +0,0 @@
-# This is named the way it is so it doesn't conflict with an existing installation, and serves as a way to showcase the layout of this botconfig.py file
-# Rename this file to botconfig.py to use it and remove these comments
-
-prefix = 'Enter your Prefix here'
-token = 'Put your token here'
-botdesc = '''The Description of the bot if being used for other uses apart from being Nep'''
