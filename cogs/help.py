58dc8d3389d0d31aba1f14afe1ed21500beeec52
Cameron Miller <cambomiller@googlemail.com>
2018-07-16 10:59:01 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -38,17 +38,49 @@ She also does normal bot stuff, because y'know, that's important too right.""", 
         since = datetime.datetime(2018, 5, 15, 0, 0)
         days_since = (datetime.datetime.utcnow() - since).days
 
-        embed = discord.Embed(title="""A Discord bot based around the lovable Neptune and her 4th wall breaking abilities.
-        She also does normal bot stuff, because y'know, that's important too right.""", colour=discord.Colour(0xb675c7))
-        embed.set_author(name="CPU Purple Heart", icon_url="https://caboosh.s-ul.eu/oSqCT9e5.png")
-        embed.add_field(name="General Commands:", inline=False)
-        embed.add_field(name="{}profile".format(prefix), value="Shows the user's profile in a nice embed", inline=False)
+        embed = discord.Embed(
+            title="""A Discord bot based around the lovable Neptune and her 4th wall breaking abilities.
+        She also does normal bot stuff, because y'know, that's important too right.""", 
+            colour=discord.Colour(0xb675c7)
+        )
+        embed.set_author(
+            name="CPU Purple Heart",
+            icon_url="https://caboosh.s-ul.eu/oSqCT9e5.png"
+        )
+        embed.add_field(
+            name="General Commands:", 
+            value="---", 
+            inline=False
+        )
+        embed.add_field(
+            name="{}profile".format(prefix), 
+            value="Shows the user's profile in a nice embed", 
+            inline=False
+        )
+        embed.add_field(
+            name="{}info".format(prefix), 
+            value="Shows information about the bot and the environment it is running in, useful for testing forks and such.", 
+            inline=False
+        )
+        embed.add_field(
+            name="{}ping".format(prefix), 
+            value="Pings the bot and returns the response time in \"0.<response time> s\" format.", 
+            inline=False
+        )
+        embed.add_field(
+            name="{}vitainfo [pg<number>]".format(prefix), 
+            value="""Shows some basic essentials for Vita Homebrew, mainly for the \"PeachProduction\" and \"Cabooshy's Den\" Discord Servers.""", 
+            inline=False
+        )
         embed.add_field(
             name="{}echo <word/string>".format(prefix),
-            value="""Echos input passed to the bot, can be used to make the bot say things""", inline=False)
+            value="""Echos input passed to the bot, can be used to make the bot say things.""", 
+            inline=False
+        )
         embed.set_footer(
             text="Breaking the 4th wall on discord since 15 May 2018 ({} days ago!)".format(days_since),
-            icon_url="https://caboosh.s-ul.eu/oSqCT9e5.png")
+            icon_url="https://caboosh.s-ul.eu/oSqCT9e5.png"
+        )
         try:
             await ctx.send(embed=embed)
         except discord.HTTPException:
@@ -74,36 +106,6 @@ She also does normal bot stuff, because y'know, that's important too right.""", 
         embed.add_field(
             name="{}botavatar".format(prefix),
             value="""Sets Nep's avatar.""", inline=False)
-
-        embed.set_footer(
-            text="Breaking the 4th wall on discord since 15 May 2018 ({} days ago!)".format(days_since),
-            icon_url="https://caboosh.s-ul.eu/oSqCT9e5.png")
-        try:
-            await ctx.send(embed=embed)
-        except discord.HTTPException:
-            await ctx.send("I need the `Embed links` permission to send this")
-
-    @help.command()
-    async def git(self, ctx):
-        since = datetime.datetime(2018, 5, 15, 0, 0)
-        days_since = (datetime.datetime.utcnow() - since).days
-
-        embed = discord.Embed(title="""A Discord bot based around the lovable Neptune and her 4th wall breaking abilities.
-           She also does normal bot stuff, because y'know, that's important too right.""",
-                              colour=discord.Colour(0xb675c7))
-        embed.set_author(name="CPU Purple Heart", icon_url="https://caboosh.s-ul.eu/oSqCT9e5.png")
-        embed.add_field(name="Git Commands: (require PyGithub)", value="---", inline=False)
-        embed.add_field(name="{}git".format(prefix), value="main command, returns \"invalid syntax\" when used alone.",
-                        inline=False)
-        embed.add_field(name="{}git push <remote, branch>".format(prefix),
-                        value="""Pushes to a repo, if a commit is available.""", inline=False)
-        embed.add_field(
-            name="{}git pull".format(prefix),
-            value="""Pulls Changes from the remote.""", inline=False)
-
-        embed.add_field(
-            name="{}git repo".format(prefix),
-            value="""Shows the current repo.""", inline=False)
 
         embed.set_footer(
             text="Breaking the 4th wall on discord since 15 May 2018 ({} days ago!)".format(days_since),
